90ca4b38c7855e5eeaefaad820728ee408cba652
Yoni Satat <yoni.satat@gmail.com>
2017-10-30 16:35:06 +0000

    created test classrommStartsEmpty - test passed

---
+++ Classroom.java
@@ -0,0 +1,17 @@
+class Classroom {
+  private Student[] classroom;
+
+  public Classroom() {
+    this.classroom = new Student[20];
+  }
+
+  public int studentCount() {
+    int count = 0;
+    for(Student student : classroom) {
+      if(student != null) {
+        count++;
+      }
+    }
+    return count;
+  }
+}
---
+++ ClassroomTest.java
@@ -0,0 +1,24 @@
+import org.junit.*;
+import static org.junit.Assert.assertEquals;
+
+public class ClassroomTest {
+  Classroom classroom;
+  Student student;
+
+  @Before
+  public void before() {
+    this.classroom = new Classroom();
+    this.student = new Student();
+  }
+
+  @Test
+  public void classroomStartsEmpty() {
+    assertEquals(0, classroom.studentCount());
+  }
+
+  @Test
+  public void classroomCanTakeStudents() {
+    classroom.add(student);
+    assertEquals(1, classroom.studentCount());
+  }
+}
